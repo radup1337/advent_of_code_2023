fd299e692aadd2e02efb67cd7bc456394c40b18f
Radu P <radu.popescu@bebop.xyz>
2023-12-01 12:42:49 +0000

    env setup

---
+++ base_solution.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod
+import os
+from typing import Any
+
+
+class BaseSolution:
+    def __init__(self, current_path: str) -> None:
+        self.__current_folder: str = os.path.dirname(current_path)
+        self.__input_path: str = os.path.join(self.__current_folder, "input.txt")
+        self.get_solution()
+
+    def __process_inputs(self) -> list[str]:
+        with open(self.__input_path, "r", encoding="utf-8") as file:
+            inputs = file.readlines()
+            return inputs
+
+    def get_solution(self) -> None:
+        inputs: list[str] = self.__process_inputs()
+
+        # Solve
+        solution = str(self.solve(inputs))
+
+        # Writing the solution to solution.txt
+        solution_path: str = os.path.join(self.__current_folder, "solution.txt")
+        with open(solution_path, "w") as file:
+            file.write(solution)
+
+    @abstractmethod
+    def solve(self, inputs: list[str]) -> Any:
+        ...
