3b63aaade46e51f766f651b4700f08cd180cd02d
Radu P <radu.popescu@bebop.xyz>
2023-12-01 13:48:46 +0000

    bit more readable

--- day_1/solve.py
+++ day_1/solve.py
@@ -5,12 +5,12 @@ from base_solution import BaseSolution
 
 class Solution(BaseSolution):
     def solve(self, inputs: list[str]) -> int:
-        numbers: list[str] = [
-            f"{[char for char in line.strip() if char.isdigit()][0]}"
-            f"{[char for char in line.strip() if char.isdigit()][-1]}"
-            for line in inputs
-        ]
-        return sum(list(map(int, numbers)))
+        total: int = 0
+        for line in inputs:
+            all_ints: list[str] = [char for char in line.strip() if char.isdigit()]
+            joint_number = int(all_ints[0] + all_ints[-1])
+            total += joint_number
+        return total
 
 
 if __name__ == "__main__":
