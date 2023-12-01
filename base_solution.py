8eeeeaf3f054d5adb7a1574165225bd56e7a2fd9
Radu P <radu.popescu@bebop.xyz>
2023-12-01 12:45:25 +0000

    make priv

--- base_solution.py
+++ base_solution.py
@@ -7,14 +7,14 @@ class BaseSolution:
     def __init__(self, current_path: str) -> None:
         self.__current_folder: str = os.path.dirname(current_path)
         self.__input_path: str = os.path.join(self.__current_folder, "input.txt")
-        self.get_solution()
+        self.__get_solution()
 
     def __process_inputs(self) -> list[str]:
         with open(self.__input_path, "r", encoding="utf-8") as file:
             inputs = file.readlines()
             return inputs
 
-    def get_solution(self) -> None:
+    def __get_solution(self) -> None:
         inputs: list[str] = self.__process_inputs()
 
         # Solve
