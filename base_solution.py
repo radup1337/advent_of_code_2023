4ec3b862a38d53fa743424db04240b76fc33e353
Radu P <radu.popescu@bebop.xyz>
2023-12-01 19:59:51 +0000

    adjust for parts

--- base_solution.py
+++ base_solution.py
@@ -8,30 +8,28 @@ class BaseSolution:
         self.__current_folder: str = os.path.dirname(current_path)
         self.__input_path: str = os.path.join(self.__current_folder, "input.txt")
         self.__test_input_path: str = os.path.join(self.__current_folder, f"test_input_{part}.txt")
+        self.__test_solution: str = test_solution
         self.__part: int = part
-        self.__test(test_solution)
-        self.__get_solution()
 
     def __process_inputs(self, input_path: str) -> list[str]:
         with open(input_path, "r", encoding="utf-8") as file:
             inputs = file.readlines()
             return inputs
 
-    def __get_solution(self) -> None:
+    def get_solution(self) -> None:
         inputs: list[str] = self.__process_inputs(self.__input_path)
-
         # Solve
         solution = str(self.solve(inputs))
-
+        print(f"Solution: {solution}")
         # Writing the solution to solution.txt
         solution_path: str = os.path.join(self.__current_folder, f"solution_{self.__part}.txt")
         with open(solution_path, "w") as file:
             file.write(solution)
 
-    def __test(self, test_solution: str) -> None:
+    def test(self) -> None:
         inputs: list[str] = self.__process_inputs(self.__test_input_path)
         solution = str(self.solve(inputs))
-        assert solution == test_solution, f"Expected {test_solution}, got {solution}"
+        assert solution == self.__test_solution, f"Expected {self.__test_solution}, got {solution}"
 
     @abstractmethod
     def solve(self, inputs: list[str]) -> Any:
--- day_1/solve.py
+++ day_1/solve.py
@@ -3,7 +3,7 @@ import os
 from base_solution import BaseSolution
 
 
-class Solution(BaseSolution):
+class SolutionPart1(BaseSolution):
     def solve(self, inputs: list[str]) -> int:
         total: int = 0
         for line in inputs:
@@ -13,5 +13,55 @@ class Solution(BaseSolution):
         return total
 
 
+class SolutionPart2(BaseSolution):
+    def __init__(self, current_path: str, part: int, test_solution: str) -> None:
+        super().__init__(current_path, part, test_solution)
+        self.word_to_int = {
+            "one": "1",
+            "two": "2",
+            "three": "3",
+            "four": "4",
+            "five": "5",
+            "six": "6",
+            "seven": "7",
+            "eight": "8",
+            "nine": "9",
+        }
+
+    def solve(self, inputs: list[str]) -> int:
+        total: int = 0
+        for line in inputs:
+            number = self.find_numbers(line.strip())
+            total += int(number)
+        return total
+
+    def find_numbers(self, line: str) -> int:
+        first_number = self.search(line, 1)
+        last_number = self.search(line[::-1], -1)
+        assert first_number and last_number
+        return int(first_number + last_number)
+
+    def search(self, line: str, reverse: int) -> str | None:
+        i: int = 0
+        search_line: str = ""
+        while i < len(line):
+            if line[i].isdigit():
+                return line[i]
+            search_line += line[i]
+            for word in self.word_to_int:
+                if word in search_line[::reverse]:
+                    return self.word_to_int[word]
+            i += 1
+        return None
+
+
 if __name__ == "__main__":
-    Solution(os.path.abspath(__file__))
+    path = os.path.abspath(__file__)
+    # ---------------------------------- Part 1 ---------------------------------- #
+    part1 = SolutionPart1(current_path=path, part=1, test_solution="142")
+    part1.test()
+    part1.get_solution()
+    # ---------------------------------- Part 2 ---------------------------------- #
+    part2 = SolutionPart2(current_path=path, part=2, test_solution="281")
+    part2.test()
+    part2.get_solution()
