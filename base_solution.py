46dd1cb9730b2800a217072157f28f67e14f29bb
Radu P <radu.popescu@bebop.xyz>
2023-12-02 11:46:02 +0000

    add expected solution to test input file

--- base_solution.py
+++ base_solution.py
@@ -4,11 +4,10 @@ from typing import Any
 
 
 class BaseSolution:
-    def __init__(self, current_path: str, part: int, test_solution: str) -> None:
+    def __init__(self, current_path: str, part: int) -> None:
         self.__current_folder: str = os.path.dirname(current_path)
         self.__input_path: str = os.path.join(self.__current_folder, "input.txt")
         self.__test_input_path: str = os.path.join(self.__current_folder, f"test_input_{part}.txt")
-        self.__test_solution: str = test_solution
         self.__part: int = part
 
     def __process_inputs(self, input_path: str) -> list[str]:
@@ -28,8 +27,9 @@ class BaseSolution:
 
     def test(self) -> None:
         inputs: list[str] = self.__process_inputs(self.__test_input_path)
-        solution = str(self.solve(inputs))
-        assert solution == self.__test_solution, f"Expected {self.__test_solution}, got {solution}"
+        expected_answer: str = inputs[0].split(":")[1].strip()
+        solution: str = str(self.solve(inputs[1:]))
+        assert solution == expected_answer, f"Expected {expected_answer}, got {solution}"
 
     @abstractmethod
     def solve(self, inputs: list[str]) -> Any:
